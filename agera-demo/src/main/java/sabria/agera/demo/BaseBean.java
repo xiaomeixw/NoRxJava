1da06ad629ec78d0fa82cfe49483aa50233c0c19
git <xiong.xiong@clever-m.com>
2016-05-28 13:08:00 +0800

    add agera-demo & test OK

---
+++ agera-demo/src/main/java/sabria/agera/demo/BaseBean.java
@@ -0,0 +1,40 @@
+package sabria.agera.demo;
+
+import com.google.android.agera.BaseObservable;
+import com.google.android.agera.UpdateDispatcher;
+
+/**
+ * Created by xiong,An android project Engineer,on 28/5/2016.
+ * Data:28/5/2016  下午 12:03
+ * Base on clever-m.com(JAVA Service)
+ * Describe:
+ * Version:1.0
+ * Open source
+ */
+public class BaseBean extends BaseObservable{
+
+
+    private String content = "";
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+        update();
+    }
+
+    private void update() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                content += " after other thread";
+                dispatchUpdate();
+            }
+        }).start();
+    }
+
+
+}
--- agera-demo/src/main/java/sabria/agera/demo/MainActivity.java
+++ agera-demo/src/main/java/sabria/agera/demo/MainActivity.java
@@ -2,14 +2,65 @@ package sabria.agera.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+import com.google.android.agera.Updatable;
+
+public class MainActivity extends Activity implements TextWatcher, Updatable {
+
+    TextView mChangeTv;
+    EditText mInputEt;
+    BaseBean baseBean;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        baseBean = new BaseBean();
+
+        mChangeTv = (TextView) findViewById(R.id.tv_change);
+        mInputEt = (EditText) findViewById(R.id.et_input);
+        mInputEt.addTextChangedListener(this);
+
     }
 
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        if (baseBean != null){
+            baseBean.setContent(s.toString());
+        }
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        baseBean.addUpdatable(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        baseBean.removeUpdatable(this);
+    }
+
+    @Override
+    public void update() {
+        mChangeTv.setText(baseBean.getContent());
+    }
 }
